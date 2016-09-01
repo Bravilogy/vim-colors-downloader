cf1316b806107e4d270131fee646d87b2630b6d9
Gega Nizharadze <Bravilogy@users.noreply.github.com>
2016-09-01 17:41:16 +0100

    Added multiprocessing

--- vimcolors.py
+++ vimcolors.py
@@ -1,7 +1,15 @@
 import os
+import time
 import json
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
+
+class Link:
+    def __init__(self, name, url):
+        self.url = url
+        self.name = name
 
 
 class Spider:
@@ -15,28 +23,39 @@ class Spider:
             # create it...
             os.mkdir(self.download_dir)
 
-    def download(self, name, url):
-        full_path = os.path.join(self.download_dir, name)
-        # If we have already downloaded this file, just skip
-        if os.path.isfile(full_path):
-            raise Exception('File: {} already exists; skipping.'.format(name))
+    def download(self, link):
+        name, url = link.name, link.url
 
-        # Get the response
-        response = requests.get(url)
-        if response.status_code == 404:
-            raise Exception('File not found: {}'.format(url))
+        try:
+            full_path = os.path.join(self.download_dir, name)
+            # If we have already downloaded this file, just skip
+            if os.path.isfile(full_path):
+                raise Exception('File: {} already exists; skipping.'.format(name))
 
-        # Try downloading the file
-        with open(full_path, 'wb') as file_path:
-            file_path.write(response.content)
+            # Get the response
+            response = requests.get(url)
+            if response.status_code == 404:
+                raise Exception('File not found: {}'.format(url))
+
+            # Try downloading the file
+            with open(full_path, 'wb') as file_path:
+                file_path.write(response.content)
+        except Exception as e:
+            print(e)
+        else:
+            print('Downloaded', name)
 
     def crawl(self):
         def repo_formatter(scheme):
             base_url = scheme['github_repo']['address'].replace('github.com', 'raw.githubusercontent.com')
             return '{}/master/colors/'.format(base_url)
+        
+        # This will hold all the links
+        collection = []
 
         # Loop over all the pages
         for page in range(self.total_pages):
+            print('Fetching links from page', page + 1, 'out of', self.total_pages)
             page_source = requests.get(self.base_url, params={'page': page + 1})
             plain_text = page_source.text
             soup = BeautifulSoup(plain_text, 'lxml')
@@ -47,14 +66,16 @@ class Spider:
             # Download the files
             for data in json_data['colorschemes']:
                 file_name = data['name'] + '.vim'
-                try:
-                    self.download(file_name, repo_formatter(data) + file_name)
-                    print('Downloaded', file_name)
-                except Exception as e:
-                    print(e)
+                collection.append(Link(file_name, repo_formatter(data)))
 
-        print('\n\nAll done.')
+        # Delegate to multiprocessing
+        print('\n\nFinished crawling pages. Starting downloads...')
+        time.sleep(2)
 
+        p = Pool()
+        p.map(self.download, collection)
+
+        print('\n\nAll done. Saved files in', os.path.abspath(self.download_dir))
 
 colors_spider = Spider()
 colors_spider.crawl()
